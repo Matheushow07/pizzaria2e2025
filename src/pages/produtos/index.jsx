c88be2efb4df58aefcc76e7729c942e6c2bce57c
Matheushow07 <rm95225@estudante.fieb.edu.br>
2025-09-15 11:30:16 +0000

    Consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,14 +1,36 @@
+import axios, {app} from 'axios'
+import { useEffect, useState } from 'react';
+
 const Produtos = () => {
    
-   
+    axios.get('http://172.19.0.49/pizzariaoficial/api/v1/produto')
+    .then((response)=>{
+        alert(JSON.stringify(response.data))
+
+    })
+
+   // Consumir os produtos endpoint (rota)
+   useEffect = (() => {
+
+}, []);
+
+    //Objeto que possui lista de pizzas
+   const pizzas = [
+        "Pizza de Frango",
+        "Pizza de Muçarela",
+        "Pizza de Calabresa",
+        "Pizza Baiana",
+        "Pizza Portuguesa",
+        "Pizza de Atum", "Pizza de Seis Queijos",
+
+   ];
+   //Mapeamento das pizzas da lista (literação)
+   const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
     return(
     <>
         <h3>Listagem de Produtos</h3>
         <ul>
-            <li>Pizza de Muçarela</li>
-            <li>Pizza de Calabresa</li>
-            <li>Pizza Baiana</li>
-            <li>Pizza Portuguesa</li>
+            {listaPizzas}
         </ul>
     </>
     )
